913d52c3ba216150fe07ed71d9dc5f411a1753f6
m1ns3c <root@minsec.cn>
2022-06-16 23:42:45 +0800

    add base sh

---
+++ tools/zsh/base.sh
@@ -0,0 +1,7 @@
+# export M1NS3C_ROOT="/data/base"
+# [ -s ${M1NS3C_ROOT}/tools/zsh/base.sh ] && source ${M1NS3C_ROOT}/tools/zsh/base.sh
+
+alias vim='emacs -Q'
+alias vi='emacs -Q'
+alias e='emacs -Q'
+alias jr='cd "$(${M1NS3C_ROOT}/tools/tools.sh -r)"'
