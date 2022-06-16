8adcfd05440cf735bf2c8c2767eec7b88bcaa225
m1ns3c <root@minsec.cn>
2022-06-16 23:49:12 +0800

    update alias and config

--- tools/zsh/base.sh
+++ tools/zsh/base.sh
@@ -1,7 +1,17 @@
 # export M1NS3C_ROOT="/data/base"
 # [ -s ${M1NS3C_ROOT}/tools/zsh/base.sh ] && source ${M1NS3C_ROOT}/tools/zsh/base.sh
 
+# git alias
+alias p='git pull'
+alias s='git push'
+alias sf='git push --force'
+alias su='git push -u origin'
+
+# editor alias
 alias vim='emacs -Q'
 alias vi='emacs -Q'
 alias e='emacs -Q'
+
+# path alias
 alias jr='cd "$(${M1NS3C_ROOT}/tools/tools.sh -r)"'
+alias jb='cd "${M1NS3C_ROOT}"'
